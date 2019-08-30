6413196095e6fe3953554041d61da4d23d2c01ea
Chris Cruz <christopherc1331@gmail.com>
2019-08-29 23:50:21 -0700

    Finished edit functionality of actions!  Woohoo!

--- randomactsapp/src/Components/Actions/ActionDetails.js
+++ randomactsapp/src/Components/Actions/ActionDetails.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
 import { axiosWithAuth } from "../AxiosAuth.js";
 
 const ActionDetails = props => {
   const [action, setAction] = useState([]);
+
+  const replaceAction = newAction => {
+    setAction([newAction]);
+  };
 
   console.log(props);
 
@@ -26,7 +29,12 @@ const ActionDetails = props => {
         <div>buttons here</div>
         {action.map(item => (
           <div key={item.id}>
-            <ActionButtons props={props} item={item} />
+            <ActionButtons
+              key={item.id}
+              replaceAction={replaceAction}
+              props={props}
+              item={item}
+            />
             <p>{item.action}</p>
           </div>
         ))}
@@ -36,7 +44,9 @@ const ActionDetails = props => {
 };
 
 const ActionButtons = props => {
-  const deleteAct = () => {
+  console.log(props);
+
+  const deleteAction = () => {
     axiosWithAuth()
       .delete(
         `https://random-acts0519.herokuapp.com/api/actions/${props.item.id}`
@@ -50,13 +60,50 @@ const ActionButtons = props => {
       });
   };
 
+  const [updatedAction, setUpdatedAction] = useState({
+    action: ""
+  });
+
+  const changeHandler = event => {
+    setUpdatedAction({
+      ...updatedAction,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const editAction = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .put(
+        `https://random-acts0519.herokuapp.com/api/actions/${props.item.id}`,
+        updatedAction
+      )
+      .then(result => {
+        console.log(result.data);
+        props.replaceAction(updatedAction);
+      })
+      .catch(err => {
+        console.log("Server Error", err);
+      });
+  };
+
   return (
     <div>
-      <button onClick={deleteAct} className="actionButtons">
+      <button onClick={deleteAction} className="actionButtons">
         Delete Act
       </button>
 
-      <button className="actionButtons">Delete Act</button>
+      <form onSubmit={editAction}>
+        <input
+          type="textarea"
+          name="action"
+          placeholder="Type Adjusted Action Here"
+          value={updatedAction.action}
+          onChange={changeHandler}
+        />
+
+        <button className="actionButtons">Edit Act</button>
+      </form>
     </div>
   );
 };
